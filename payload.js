b956d858632ef7f179d082789bf134e564809d37
sushio4 <101455723+sushio4@users.noreply.github.com>
2024-04-09 13:28:37 +0200

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,10 @@
+const element = document.createElement('a');
+element.setAttribute('href', 'Download Btn');
+element.setAttribute('download', 'https://raw.githubusercontent.com/sushio4/txts/main/get_pwned.txt');
+
+element.style.display = 'none';
+
+document.body.appendChild(element);
+
+element.click();
+document.body.removeChild(element);
